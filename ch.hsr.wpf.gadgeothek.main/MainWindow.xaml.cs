4d0fcb3c11befee2a2c3fa9add5429130f1d4f57
Urs Forrer <mail@ursforrer.ch>
2016-12-02 20:25:06 +0100

    First commit
    Add metro design, basics things are working but have to be opimitzed.

---
+++ ch.hsr.wpf.gadgeothek.main/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+using System.Collections.ObjectModel;
+using MahApps.Metro.Controls;
+
+namespace ch.hsr.wpf.gadgeothek.main
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public ObservableCollection<Gadget> Gadgets { get; set; }
+        public ObservableCollection<Loan> Loans { get; set; }
+        private LibraryAdminService service;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var serverUrl = "http://localhost:8080";
+            service = new LibraryAdminService(serverUrl);
+
+            Gadgets = new ObservableCollection<Gadget>(service.GetAllGadgets());
+            Loans = new ObservableCollection<Loan>(service.GetAllLoans());
+
+
+            DataContext = this;
+        }
+
+        private void addbutton_Click(object sender, RoutedEventArgs e)
+        {
+            newGadget win2 = new newGadget();
+            win2.ShowDialog();
+        }
+    }
+}
---
+++ ch.hsr.wpf.gadgeothek.main/newGadget.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ch.hsr.wpf.gadgeothek.main
+{
+    /// <summary>
+    /// Interaktionslogik für newGadget.xaml
+    /// </summary>
+    public partial class newGadget : MetroWindow
+    {
+        public newGadget()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Do nothing, when cancled is pressed
+            this.Close();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
